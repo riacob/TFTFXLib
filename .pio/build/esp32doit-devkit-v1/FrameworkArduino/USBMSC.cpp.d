802f967b8c60b4ff1160c3d379648519372a60de
iacob <iacob.riccardo@gmail.com>
2023-07-12 06:54:10 +0200

    rearrange files

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/iacob/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/iacob/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/iacob/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qspi_qspi/include/sdkconfig.h
