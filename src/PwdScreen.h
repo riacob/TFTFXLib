4d7763a4872cefef8ada8b582111cc94b2e734f6
iacob <iacob.riccardo@gmail.com>
2023-07-12 22:59:26 +0200

    continue development of pwdscreen

--- src/PwdScreen.h
+++ src/PwdScreen.h
@@ -47,7 +47,7 @@ public:
         /* MAIN USER CODE BEGIN */
 
         FXButton btn1(w);
-        /*FXButton btn2(w);
+        FXButton btn2(w);
         FXButton btn3(w);
         FXButton btn4(w);
         FXButton btn5(w);
@@ -62,15 +62,53 @@ public:
         FXButton btnD(w);
         FXButton btnSTAR(w);
         FXButton btnHASH(w);
-        FXButton textBox(w);*/
+        FXButton textBox(w);
 
-        btn1.setPosition(0, 50);
+        // row1
+        btn1.setPosition(0, 100);
         btn1.setSize(50, 50);
-        btn1.setButtonBackgroundColor(TFT_BLACK);
+        btn1.setButtonBackgroundColor(TFT_BLUE);
+        btn1.setButtonText("1", 2, 2);
 
-        /*btn0.draw();*/
+        btn2.setPosition(50, 100);
+        btn2.setSize(50, 50);
+        btn2.setButtonBackgroundColor(TFT_BLUE);
+        btn2.setButtonText("2", 2, 2);
+
+        btn3.setPosition(100, 100);
+        btn3.setSize(50, 50);
+        btn3.setButtonBackgroundColor(TFT_BLUE);
+        btn3.setButtonText("3", 2, 2);
+
+        btnA.setPosition(150, 100);
+        btnA.setSize(50, 50);
+        btnA.setButtonBackgroundColor(TFT_BLUE);
+        btnA.setButtonText("A", 2, 2);
+
+        //row2
+        btn4.setPosition(0, 150);
+        btn4.setSize(50, 50);
+        btn4.setButtonBackgroundColor(TFT_BLUE);
+        btn4.setButtonText("4", 2, 2);
+
+        btn5.setPosition(50, 150);
+        btn5.setSize(50, 50);
+        btn5.setButtonBackgroundColor(TFT_BLUE);
+        btn5.setButtonText("5", 2, 2);
+
+        btn6.setPosition(100, 150);
+        btn6.setSize(50, 50);
+        btn6.setButtonBackgroundColor(TFT_BLUE);
+        btn6.setButtonText("6", 2, 2);
+
+        btnB.setPosition(150, 150);
+        btnB.setSize(50, 50);
+        btnB.setButtonBackgroundColor(TFT_BLUE);
+        btnB.setButtonText("B", 2, 2);
+
+        btn0.draw();
         btn1.draw();
-        /*btn2.draw();
+        btn2.draw();
         btn3.draw();
         btn4.draw();
         btn5.draw();
@@ -86,22 +124,22 @@ public:
         btnHASH.draw();
         textBox.draw();
 
-        btn0.touchAt(touchx,touchy);*/
-        btn1.touchAt(touchx,touchy);
-        /*btn2.touchAt(touchx,touchy);
-        btn3.touchAt(touchx,touchy);
-        btn4.touchAt(touchx,touchy);
-        btn5.touchAt(touchx,touchy);
-        btn6.touchAt(touchx,touchy);
-        btn7.touchAt(touchx,touchy);
-        btn8.touchAt(touchx,touchy);
-        btn9.touchAt(touchx,touchy);
-        btnA.touchAt(touchx,touchy);
-        btnB.touchAt(touchx,touchy);
-        btnC.touchAt(touchx,touchy);
-        btnD.touchAt(touchx,touchy);
-        btnSTAR.touchAt(touchx,touchy);
-        btnHASH.touchAt(touchx,touchy);*/
+        btn0.touchAt(touchx, touchy);
+        btn1.touchAt(touchx, touchy);
+        btn2.touchAt(touchx, touchy);
+        btn3.touchAt(touchx, touchy);
+        btn4.touchAt(touchx, touchy);
+        btn5.touchAt(touchx, touchy);
+        btn6.touchAt(touchx, touchy);
+        btn7.touchAt(touchx, touchy);
+        btn8.touchAt(touchx, touchy);
+        btn9.touchAt(touchx, touchy);
+        btnA.touchAt(touchx, touchy);
+        btnB.touchAt(touchx, touchy);
+        btnC.touchAt(touchx, touchy);
+        btnD.touchAt(touchx, touchy);
+        btnSTAR.touchAt(touchx, touchy);
+        btnHASH.touchAt(touchx, touchy);
 
         /* MAIN USER CODE END */
 
